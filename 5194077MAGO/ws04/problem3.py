645fee882b7000375a2a8716f768cda2294712a6
Murilo Oliveira <murilo.oliveira7@gmail.com>
2021-04-16 13:09:57 +1000

    Workshop 4 | Updating comments on problem 3

--- 5194077MAGO/ws04/problem3.py
+++ 5194077MAGO/ws04/problem3.py
@@ -4,10 +4,18 @@
 #Recieve a number from the user to start the loop
 number = (int(input("Enter a positive number: ")))
 
+#Check if the provided number is valid
 if number > 0:
+    #Do the top of the dimond
     for i in range(number):
+        #Row will be "numbre-1" spaces and "i*2+1" * 
+        #first row for number=5 will be 4 spaces (5-1=4) and 1 * (0*2+1=1)
         print(" "*(number-i), "*"*(i*2+1))
+
+    #Do the botton of the dimond starting after the middle row
     for i in range(number-2, -1, -1):
+        #On our example set i to start on 3 (number-2)
+        #to do the inverse dimond and complete the botton
         print(" "*(number-i), "*"*(i*2+1))
 else:
     print("Invalid number")
